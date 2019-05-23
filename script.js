112ded7fd8c7156f3079a3cfe070d61523265ce5
Silviu <silviu.iordache92@gmail.com>
2019-05-23 13:09:25 +0300

    progress
    stuff

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 class Task {
-    constructor(id, title, desc) {
+    constructor(id, title) {
         this.id = id;
         this.title = title;
-        this.desc = desc;
-    }
+    } 
 }
 
-// Arrays that hold the active and completed tasks
-let activeTasks = [];
-let completedTasks = [];
+active = [];
+completed = [];
 
-//dummy tasks input - to be replaced with the data from the input fields
-let titles = ["Clean room", "Fix car", "Gardening", "Groceries"];
-let descriptions = ["des1", "des2", "des3", "des4"];
-//let prios = ["high", "med", "low", "med"];
+function populate() {
+    //dummy tasks input - to be replaced with the data from the input fields
+    let titlesActive = ["Clean room", "Fix car", "Gardening", "Groceries"];
+    let titlesCompleted = ["Sleep", "Play"];
 
-// Quick creation and insertion of tasks data into the array
-for (let i = 0; i < 4; i++) {
-    addTask(titles[i], descriptions[i], activeTasks);
+    // populate "active" tasks list
+    titlesActive.forEach(e => createTask(e, active));
+    deleteTask(1, active);
+    createTask("Go running", active);
+
+    // also populate "completed" tasks list
+    titlesCompleted.forEach(e => createTask(e, completed));
 }
 
-// add new task to one of the Task arrays
-function addTask(title, desc, arr) {
-    // generate unique id for each task obj (independent of array index)
+// finds the array index of a task by id
+function getIndexOfId(id, arr) {
+    return arr.findIndex(el => el.id === id);
+}
+
+// create a new task and add it to the target array
+function createTask(title, arr) {
+    // generate unique ids for each task obj
     let id;
     if (arr.length === 0) {
         //id = Math.round(Math.random() * 10);
@@ -30,37 +37,79 @@ function addTask(title, desc, arr) {
     } else {
         id = arr[arr.length - 1].id + 1;
     }
-    const task = new Task(id, title, desc, );
+    const task = new Task(id, title);
+
+    // add task to the array
     arr.push(task);
+
+    // add task to the DOM
+    addTask_UI(id, title);
 }
 
-// finds the index of a task with id = idTarget
-function getIndexOfId(idTarget, arr) {
-    return arr.findIndex(el => el.id === idTarget);
+// edit a task
+function editTask(id, title, arr) {
+    const index = getIndexOfId(id, arr);
+    arr[index].title = title;
 }
 
-// deletes the task with id = idToDel
-function deleteTask(idToDel, arr) {
-    const index = getIndexOfId(idToDel, arr);
+// deletes a task by id
+function deleteTask(id, arr) {
+    const index = getIndexOfId(id, arr);
     if (index != -1) {
         arr.splice(index, 1);
     }
 }
 
+// complete a task by id
 function completeTask(id) {
-    const index = getIndexOfId(id, activeTasks);
+    const index = getIndexOfId(id, active);
+    if (index === -1) {
+        console.log(`Trying to complete inexistent task of id ${id}`);
+    } else {
+        // create a copy of the task obj to be deleted
+        let tempObj = JSON.parse(JSON.stringify(active[index]));
 
-    // create a copy of the task obj to be deleted
-    let o = JSON.parse(JSON.stringify(activeTasks[index]));
+        // extract it's data
+        let tempTitle = tempObj.title;
 
-    // extract it's data
+        // create a new Task with the same title and add it to the completed tasks
+        createTask(tempTitle, completed)
 
-    // create a new Task obj
-    addTask(o.id, )
-    completedTasks.push(completedTask);
-    deleteTask(index, activeTasks);
+        // delete the task from the active list
+        deleteTask(id, active);
+    }
 }
-//deleteTask(2, activeTasksArr);
+
+const DOM = {
+    form: document.getElementById("form"),
+    input:document.getElementById("input"),
+    btn: document.getElementById("btn"),
+    list: document.getElementById("list")
+}
+
+// button event listener
+DOM.btn.addEventListener('click', createTask);
+
+function addTask_UI(id, text) {
+    //var id = 3;
+    //var text = "alabala";
+    const item = `<li id="li-${id}">${text}<input id="box-${id}" class="checkboxes" type="checkbox"></li>`
+
+    DOM.list.insertAdjacentHTML('beforeend', item);
+}
+
+function initializeList_UI() {
+
+}
+
+
+
+//populate();
+
+
+
+
+
 
 
 
