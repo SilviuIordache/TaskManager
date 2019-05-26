268a0f6ce2db9c8f87764b74003f285e53e7524b
Silviu <silviu.iordache92@gmail.com>
2019-05-27 02:35:34 +0300

    final touches

--- script.js
+++ script.js
@@ -48,32 +48,34 @@ function addTaskToUI(task, DOMList) {
   const id = task.id;
   const title = task.title;
   const desc = task.desc;
+  let checked;
 
-  //const item = `<li id="li-${id}" class="text-wrap li_${DOMList}">${title}
-  //<input id="box-${id}" class="checkboxes" type="checkbox"></li>`
+  // if it's a completed task, the checked state will be inserted into the li DOM text;
+  checked = (DOMList === "completed") ? 'checked="checked"' : ''
 
-  const item = `<li id="li-${id}" class="text wrap li_${DOMList}"><div class="container"><div class="row"><div class="col"><div class="container"><div class="task-title-and-desc col"><h5 class="task-title row">${title}</br></h5><h7 class="task-desc row">${desc}</div></div></div><div class="task-buttons align-items-start col-3"><button type="button" class="close icon-white row"aria-label="Close"><span aria-hidden="true" id="closebtn-${DOMList}-${id}">&times;</span></button><div class="checkbox-container float-right row"><input class="checkboxes" id="checkbox-${DOMList}-${id}" type="checkbox"></div></div></div></div></li>`
+  const item = `<li id="li-${id}-${DOMList}" class="text wrap li_${DOMList}"><div class="container"><div class="row"><div class="col"><div class="container"><div class="task-title-and-desc col"><h5 class="task-title row">${title}</br></h5><h7 class="task-desc row">${desc}</div></div></div><div class="task-buttons align-items-start col-3"><button type="button" class="close icon-white row"aria-label="Close"><span aria-hidden="true" id="closebtn-${DOMList}-${id}">&times;</span></button><div class="checkbox-container float-right row"><input class="checkboxes" ${checked} id="checkbox-${DOMList}-${id}" type="checkbox"></div><div  class="edit-btn row">
+  <button type="button" id="editbtn-${DOMList}-${id}" class="btn-outline-info">Edit</button>
+</div></div></div></div></li>`;
 
-  // if (DOMList === "active") {
-  //   DOM.list_active.insertAdjacentHTML('beforeend', item);
-  // } else if (DOMList === "completed") {
-  //   DOM.list_completed.insertAdjacentHTML('beforeend', item);
-  // }
 
   DOM[DOMList].insertAdjacentHTML('beforeend', item);
 
   // Clear Form input fields
-  DOM.title_input.value = "";
-  DOM.desc_input.value = "";
+  clearInputFields();
 
   // Set focus back on the first input field (title)
   DOM.title_input.focus();
 }
 
 function delTaskFromUI(id, DOMList) {
-  const li = document.getElementById(`li-${id}`);
+  const li = document.getElementById(`li-${id}-${DOMList}`);
   const ul = document.getElementById(`list_${DOMList}`);
   ul.removeChild(li);
+}
+
+function updateTasksNumber() {
+  DOM.active_title.textContent = `Active (${taskArr.active.length})`;
+  DOM.completed_title.textContent = `Completed (${taskArr.completed.length})`;
 }
 
 
@@ -87,43 +89,42 @@ let taskArr = {
 }
 
 function init() {
-  getTasksData();
+  getLocalStorageData();
+  addStoredTasksToUI();
+  updateTasksNumber();
 }
 
-// get tasks from json
-function getTasksData() {
-  var request = new XMLHttpRequest();
-  request.open("GET", "tasks.json");
-  request.onreadystatechange = function () {
-    if (this.readyState == this.DONE && this.status == 200) {
-      if (this.responseText) {
-        parseTasks(this.responseText);
-        addStoredTasksToUI();
-      } else {
-        console.log("Error: Data is empty");
-      }
-    }
-  };
-  request.send();
-}
+// retrieves localStorage data and puts it in tasksArr
+function getLocalStorageData() {
+  let data = JSON.parse(localStorage.getItem("info"));
 
-// push data from json to the task arrays
-function parseTasks(tasksJSON) {
-  //console.log(tasksJSON);
-  data = JSON.parse(tasksJSON);
-  //console.log(data);
+  if (data === null) {
+    console.log('empty local storage');
+  } else {
 
-  data.forEach((e) => {
-    let nTask = new Task(e.id, e.title, e.desc, e.completed)
-
-    if (nTask.completed === true) {
-      taskArr.completed.push(nTask);
+    // check if active has something stored in it
+    if (data.active.length === 0) {
+      console.log('no active tasks in localStorage');
     } else {
-      taskArr.active.push(nTask);
+      data.active.forEach((e) => {
+        let nTask = new Task(e.id, e.title, e.desc, e.completed)
+        taskArr.active.push(nTask);
+      })
     }
-  })
+
+    // check if completed has something stored in it
+    if (data.completed.length === 0) {
+      console.log('no completed tasks in localStorage')
+    } else {
+      data.completed.forEach((e) => {
+        let nTask = new Task(e.id, e.title, e.desc, e.completed)
+        taskArr.completed.push(nTask);
+      })
+    }
+  }
 }
 
+// populate DOM with stored data
 function addStoredTasksToUI() {
   if (taskArr.active.length > 0) {
     for (item of taskArr.active) {
@@ -135,6 +136,11 @@ function addStoredTasksToUI() {
       addTaskToUI(item, "completed");
     }
   }
+}
+
+function clearInputFields() {
+  DOM.title_input.value = "";
+  DOM.desc_input.value = "";
 }
 
 function createTask() {
@@ -152,34 +158,71 @@ function createTask() {
     const id = genIdForArr(taskArr.active);
 
     // 3. Create the task obj
-    const task = new Task(id, title, desc);
+    const task = new Task(id, title, desc, false);
 
     // 4 Add task to lists
-    addTask(task, "active")
-
+    addTask(task, "active");
   }
 
 }
 
-function addTask(task, type) {
+function addTask(task, list) {
   // 1. add task to the array
-  addTaskToArr(task, taskArr[type]);
+  addTaskToArr(task, taskArr[list]);
 
   // 2. add task to the DOM
-  addTaskToUI(task, type);
+  addTaskToUI(task, list);
+
+  // 3. update localStorage
+  updateLocalStorage();
+
+  // 4. update lists numbers
+  updateTasksNumber();
 }
 
-function deleteTask(id, listType) {
+function deleteTask(id, list) {
 
   // 1. delete task from array
-  delTaskFromArr(id, taskArr[listType]);
+  delTaskFromArr(id, taskArr[list]);
 
   // 2. delete task from UI
-  delTaskFromUI(id, listType)
+  delTaskFromUI(id, list)
+
+  // 3. update local storage
+  updateLocalStorage();
+
+  // 4. update lists numbers
+  updateTasksNumber();
 }
 
+function editTask(id, list) {
 
-function moveTaskFromTo(event, srcListType, destListType) {
+  // save task title & desc
+  let index = getIndexOfId(id, taskArr[list]);
+
+  //retrieve task
+  let task = taskArr[list][index];
+
+  // delete task (from arr and DOM)
+  deleteTask(id, list);
+
+  // put it into the input fields
+  DOM.title_input.value = task.title;
+  DOM.desc_input.value = task.desc;
+
+  // update local storage
+  updateLocalStorage();
+
+}
+
+// push taskArr data into the localStorage
+function updateLocalStorage() {
+  localStorage.clear();
+  let tasks = JSON.stringify(taskArr);
+  localStorage.setItem("info", tasks);
+}
+
+function moveTaskFromTo(event, srcList, destList) {
   // id example in the dom <li id="li-active-3"></li>
 
   // 1. retrieve the id of the li from the HTML
@@ -187,7 +230,7 @@ function moveTaskFromTo(event, srcListType, destListType) {
   //console.log(id);
 
   // 2. retrieve the array index of id in srcList
-  let index = getIndexOfId(id, taskArr[srcListType]);
+  let index = getIndexOfId(id, taskArr[srcList]);
 
   // 3. validate if index found exists in the array
   if (index === -1) {
@@ -195,33 +238,30 @@ function moveTaskFromTo(event, srcListType, destListType) {
   } else {
 
     // 4. create a copy of the task obj to be deleted
-    let tempObj = JSON.parse(JSON.stringify(taskArr[srcListType][index]));
+    let tempObj = JSON.parse(JSON.stringify(taskArr[srcList][index]));
 
     // 5. extract it's data
     let tempTitle = tempObj.title;
     let tempDesc = tempObj.desc;
 
     // 6. delete task from source list
-    deleteTask(id, srcListType);
+    deleteTask(id, srcList);
 
     // 7. Generate a new unique id for the array where the task will be inserted
-    let newId = genIdForArr(taskArr[destListType]);
+    let newId = genIdForArr(taskArr[destList]);
 
     // 8. create a new Task with the same data
     let task = new Task(newId, tempTitle, tempDesc);
 
-    // 9. Add task to destination array
-    addTaskToArr(task, taskArr[destListType]);
-
-    // 10. Add task to the destination DOM list
-    addTaskToUI(task, destListType);
+    // 9. Add task
+    addTask(task, destList);
   }
 }
 
 function toggleTaskCompletion(event) {
   // id examples from DOM
-    // delete button: id="closebtn-active-3"
-    // checkbox button: id="checkbox-completed-2"
+  // delete button: id="closebtn-active-3"
+  // checkbox button: id="checkbox-completed-2"
 
   // retrieve src list type from DOM elem
   let srcList = event.target.id.split('-')[1];
@@ -233,7 +273,6 @@ function toggleTaskCompletion(event) {
 }
 
 function taskItemButtonPresssed(event) {
-
   // destructure the id of the button pressed to extract data
   let eventCallComponents = event.target.id.split('-');
 
@@ -241,15 +280,16 @@ function taskItemButtonPresssed(event) {
   let listType = eventCallComponents[1];
   let id = parseInt(eventCallComponents[2], 10);
 
-  if (buttonType === "closebtn") {
-    //if it's a close button, we delete the task
-    deleteTask(id, listType);
 
+  if (buttonType === "closebtn") {
+    deleteTask(id, listType);
   } else if (buttonType === "checkbox") {
-    //if it's a checkbox, we perform the task completion logic
     toggleTaskCompletion(event);
+  } else if (buttonType === "editbtn") {
+    editTask(id, listType);
   }
 }
+
 
 const DOM = {
   active: document.getElementById("list_active"),
@@ -259,7 +299,9 @@ const DOM = {
   desc_input: document.getElementById("desc"),
   create_task: document.getElementById("create_task"),
   checkbox_btn: document.getElementById("checkbox-btn"),
-  close_btn: document.getElementById("close-btn")
+  close_btn: document.getElementById("close-btn"),
+  active_title: document.querySelector('.active-title'),
+  completed_title: document.querySelector('.completed-title')
 }
 
 // --- Event Listeners -------
@@ -271,4 +313,9 @@ document.addEventListener('keypress', (event) => {
 })
 DOM.create_task.addEventListener('click', createTask);
 DOM.active.addEventListener('click', taskItemButtonPresssed);
-DOM.completed.addEventListener('click', taskItemButtonPresssed)
+DOM.completed.addEventListener('click', taskItemButtonPresssed);
+
+
+
+
+
